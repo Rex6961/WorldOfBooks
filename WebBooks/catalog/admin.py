dab56924a2fc2910b45aeac07cf2ab9c4d0a4092
Oleg <118850986+Rex6961@users.noreply.github.com>
2024-06-23 12:32:09 -0300

    The admin panel has been configured in the admin.py file. Added search filters, the ability to change the status of books by availability, in stock.

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -6,18 +6,32 @@ from .models import (
 )
 
 # Register your models here.
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name',
+              ('date_of_birth', 'date_of_death')]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'genre', 'language', 'display_author')
     list_filter = ('genre', 'author')
+    inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('book', 'status')
+    fieldsets = (
+        ('Экземпляр книги', {
+            'fields': ('book', 'imprint', 'inv_nom')
+        }),
+        ('Статус и окончание его действия', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 
 admin.site.register(Genre)
