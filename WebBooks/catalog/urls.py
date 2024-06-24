7343ee6c4fdb7951e765dbb622a3d6ff6a36e292
Oleg <118850986+Rex6961@users.noreply.github.com>
2024-06-24 01:20:53 -0300

    Added the page of base template, the page of books list and the page of book detail

--- WebBooks/catalog/urls.py
+++ WebBooks/catalog/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from .views import index
+from django.urls import path, re_path
+from catalog import views
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    re_path(r"^books/$", views.BookListView.as_view(), name='books'),
+    re_path(r"^book/(?P<pk>\d+)$", views.BookDetailView.as_view(), name="book-detail"),
 ]
--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
 def index(request):
-    num_books = Book.objects.all().count()
-    num_instances = BookInstance.objects.all().count()
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
     num_instances_available = BookInstance.objects.filter(status__exact=2).count()
     num_authors = Author.objects.count()
     return render(request, 'index.html', context={
@@ -15,3 +16,12 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors
     },)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
